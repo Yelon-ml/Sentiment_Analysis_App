282ed595b26e71fcc816f9d04b76c0549d299d9e
Yelon-ml <mikolaj.babula@gmail.com>
2020-10-28 22:29:56 +0100

    text_preprocessing class implemented,optimizing existing functionalities

--- sentiment_analysis__nltk_and_rnn.py
+++ sentiment_analysis__nltk_and_rnn.py
@@ -1,146 +1,211 @@
 import tensorflow as tf
 import keras
 import pandas as pd
-import re
-from string import punctuation
-import nltk
-import numpy as np
-from sklearn.metrics import accuracy_score
-from nltk.corpus import stopwords
-from collections import Counter
-from functools import reduce
-from nltk.stem import WordNetLemmatizer
-from sklearn.model_selection import train_test_split
+
+
 
 tweets = pd.read_csv(r"files\Tweets.csv", usecols=['tweet_id', 'airline_sentiment', 'text'])
 tweets = tweets[tweets.airline_sentiment != 'neutral']
 tweets.reset_index(inplace=True)
 
-reviews, labels = [], []
-for i in range(len(tweets)):
-    reviews.append(str(tweets.text[i]))
-    labels.append(1 if tweets.airline_sentiment[i]=="positive" else 0)
+reviews = [str(sentence) for sentence in tweets.text]
+labels = [1 if label=='positive' else 0 for label in tweets.airline_sentiment]
 
-reviews = [re.sub("@", " ", review) for review in reviews]
-reviews = [review.lower() for review in reviews]
-reviews = [re.sub(r'[\W]', " ", review) for review in reviews]
-reviews = [re.sub('virginamerica', "", review) for review in reviews]
-reviews = [re.sub('united', "", review) for review in reviews]
-reviews = [re.sub('americanair', "", review) for review in reviews]
-#reviews = [re.sub("  ", "", review) for review in reviews]
-for i in range(len(reviews)):
-    reviews[i] = ' '.join(word for word in reviews[i].split() if word not in stopwords.words('english'))
-lemmatizer = WordNetLemmatizer()
-reviews = [' '.join([lemmatizer.lemmatize(word) for word in review.split()]) for review in reviews]
+reviews[:6]
+
+class text_preprocessing:
+
+    def __init__(self, reviews, labels):
+        self.reviews = reviews
+        self.labels = labels
+
+    def text_cleaning(self, arg):
+
+        import re
+        import nltk
+        import numpy as np
+        from string import punctuation
+        from nltk.corpus import stopwords
+        from nltk.stem import WordNetLemmatizer
+
+        self.reviews = [re.sub("@\S+", "", review) for review in arg]
+        print("Removed terms that start with @.")
+        self.reviews = [re.sub("(?:(http|s?ftp):\/\/\S+)?", "", review) for review in self.reviews]
+        print("Removed terms that start with http://.")
+        self.reviews = [re.sub("#\S+", "", review) for review in self.reviews]
+        print("Removed terms that start with #.")
+        self.reviews = [re.sub("&\S+", "", review) for review in self.reviews]
+        print("Removed terms that start with &.")
+        self.reviews = [review.lower() for review in self.reviews]
+        print("Converted all capital letters to lower ones.")
+        self.reviews = [re.sub(r'[\W]', " ", review) for review in self.reviews]
+        print("Removed all terms that are not known as true words")
+        self.reviews = [re.sub('virginamerica', "", review) for review in self.reviews]
+        self.reviews = [re.sub('united', "", review) for review in self.reviews]
+        self.reviews = [re.sub('americanair', "", review) for review in self.reviews]
+        print("Removed terms that consider air lines.")
+        self.reviews = [' '.join(word for word in self.reviews[i].split() if word not in stopwords.words('english')) for i in range(len(self.reviews))]
+        print("Removed most common english words.")
+        self.reviews = [' '.join([WordNetLemmatizer().lemmatize(word) for word in review.split()]) for review in self.reviews]
+        print("Converted all terms into their root form.")
+
+    def text_length(self, arg, show=False, count=False, replace=False, show_final_dim=False):
+        self.len_of_review = [len(review.split()) for review in arg]
+        self.max_len_of_review = max(self.len_of_review)
+        self.min_len_of_review = min(self.len_of_review)
+        self.avg_len_of_review = np.float(sum(self.len_of_review) / len(self.reviews))
+        if show==True:
+            print("\nAverage length of review: {},\nMax length: {},\nMinimum length {}".format(avg_len_of_review, max_len_of_review, min_len_of_review))
+        if count==True:
+            count_zeros = sum([1 if zero==0 else 0 for zero in self.len_of_review])
+            print("\nThere is {} reviews of length zero.".format(count_zeros))
+        if replace==True:
+            self.reviews_and_labels = pd.DataFrame({'Reviews': self.reviews, 'Labels': self.labels, "Length_of_Review": self.len_of_review})
+            self.reviews_and_labels = self.reviews_and_labels[self.reviews_and_labels.Length_of_Review != 0]
+            self.reviews = reviews_and_labels.Reviews.to_list()
+            self.labels = reviews_and_labels.Labels.to_list()
+            if show_final_dim==True:
+                print("\nReviews dimensionality: {}\nLabels dimensionality: {}.".format(np.array(self.reviews).shape, np.array(self.labels).shape))
+
+    def dictionary(self, arg):
+        from functools import reduce
+        self.list_of_words = [[word for word in review.split()] for review in arg]
+        self.list_of_words = reduce(lambda x,y: x+y, self.list_of_words)
+        self.list_of_words = np.unique(self.list_of_words)
+        self.dictio = {word: idx for (idx, word) in enumerate(self.list_of_words)}
+        self.dictio_revers = dict((b, a) for (a, b) in self.dictio.items())
+        self.encoded_reviews = [[dictio[word] for word in review.split()] for review in arg]
+
+    def length_distribution(self, arg):
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
+        fig.suptitle("Distribution of sententce's length.")
+        sns.distplot(self.len_of_review, axlabel='Length of sentence', kde=False, hist=True, ax=ax1)
+        sns.distplot(self.len_of_review, axlabel='Length of sentence', kde=True, hist=True, ax=ax2)
+        plt.show()
+
+    def text_padding(self, arg):
+        self.seq_len = int(input("Set the length of reviews.\nAll sentences are going to be trim/pad to set value\n: "))
+        self.reviews_with_padding = np.array(np.zeros(len(arg)*self.seq_len))
+        self.reviews_with_padding = self.reviews_with_padding.reshape(len(arg), self.seq_len)
+
+        for idx, review in enumerate(arg):
+            length = len([word for word in review.split()])
+            if length < self.seq_len:
+                n = self.seq_len - length
+                zero_padding = np.zeros(n).tolist()
+                zero_padding = zero_padding + [dictio[word] for word in review.split()]
+                self.reviews_with_padding[idx] = zero_padding
+            else:
+                self.reviews_with_padding[idx]= [dictio[word] for word in review.split()[:self.seq_len]]
+
+        self.reviews_with_padding = np.array(self.reviews_with_padding)
+        print("All sentences have been trimmed/padded to the set length {}.".format(self.seq_len))
+
+    def train_val_test_split(self, x, y, test_size, val_size):
+        from sklearn.model_selection import train_test_split
+        x = np.array(x)
+        y = np.array(y)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(x, y, test_size=test_size)
+        self.X_train, self.X_val, self.y_train, self.y_val = train_test_split(self.X_train, self.y_train, test_size=val_size)
+        print("Train samples dimensionality: features - {}, labels - {}.".format(self.X_train.shape, self.y_train.shape))
+        print("Validation samples dimensionality: features - {}, labels - {}.".format(self.X_val.shape, self.y_val.shape))
+        print("Test samples dimensionality: features - {}, labels - {}.".format(self.X_test.shape, self.y_test.shape))
 
 
-len_of_review = [len(review.split()) for review in reviews]
-max_len_of_review = max(len_of_review)
-min_len_of_review = min(len_of_review)
-avg_len_of_review = np.float(sum(len_of_review) / len(reviews))
 
-print("Average length of review: {},\nMax length: {},\nMinimum length {}".format(avg_len_of_review, max_len_of_review, min_len_of_review))
-
-reviews_and_labels = pd.DataFrame({'Reviews': reviews, 'Labels': labels, "Length_of_Review": len_of_review})
-reviews_and_labels = reviews_and_labels[reviews_and_labels.Length_of_Review != 0]
-
-reviews_and_labels.head()
-
-reviews = list(reviews_and_labels.Reviews)
-labels = list(reviews_and_labels.Labels)
-len(reviews)
-len(labels)
-
-list_of_words = []
-for review in reviews:
-    list_of_words.append([word for word in review.split()])
-
-list_of_words = reduce(lambda x,y: x+y, list_of_words)
-list_of_words = np.unique(list_of_words)
-len(list_of_words)
-dictio = {word: idx for (idx, word) in enumerate(list_of_words)}
-dictio_revers = dict((b, a) for (a, b) in dictio.items())
-
-encoded_reviews = []
-for review in reviews:
-    encoded_reviews.append([dictio[word] for word in review.split()])
-
-seq_len = max_len_of_review
-reviews_with_padding = np.array(np.zeros(len(reviews)*seq_len))
-reviews_with_padding = reviews_with_padding.reshape(len(reviews), seq_len)
+data = text_preprocessing(reviews, labels)
+data.text_cleaning(data.reviews)
+data.text_length(data.reviews, show=True, count=True, replace=True, show_final_dim=True)
+data.dictionary(data.reviews)
+data.length_distribution(data.reviews)
+data.text_padding(data.reviews)
+data.reviews_with_padding.shape
+data.train_val_test_split(data.reviews_with_padding, data.labels, test_size=0.05, val_size=0.15)
+data.reviews_with_padding[:10]
 
 
-for idx, review in enumerate(reviews):
-    length = len([word for word in review.split()])
-    if length < seq_len:
-        n = seq_len - length
+
+from sklearn.metrics import accuracy_score
+
+
+
+
+
+import datetime
+
+def gru_training():
+    print("Creating model...")
+    inputs = keras.Input(shape=(26,), name='input_layer')
+    print(inputs.shape)
+    embedding = keras.layers.Embedding(len(list_of_words), 2000)(inputs)
+    print(embedding.shape)
+    gru_1 = keras.layers.GRU(256, return_sequences=True, dropout=0.2, recurrent_dropout=0.1)(embedding)
+    gru_2 = keras.layers.GRU(128, return_sequences=False, dropout=0.2, recurrent_dropout=0.1)(gru_1)
+    drop_1 = keras.layers.Dropout(0.4)(gru_2)
+    dense_1 = keras.layers.Dense(64, activation='elu')(drop_1)
+    drop_2 = keras.layers.Dropout(0.4)(dense_1)
+    dense_2 = keras.layers.Dense(32, activation='elu')(drop_2)
+    drop_3 = keras.layers.Dropout(0.4)(dense_2)
+    dense_3 = keras.layers.Dense(16, activation='elu')(drop_3)
+    drop_4 = keras.layers.Dropout(0.4)(dense_3)
+    outputs = keras.layers.Dense(1, activation='sigmoid')(drop_4)
+
+    model_with_gru = keras.Model(inputs=inputs, outputs=outputs, name="sentiment_by_gru")
+    return model_with_gru
+
+tf.keras.backend.clear_session()
+
+gru_training()
+
+model_with_gru
+
+model_with_gru.compile(optimizer='RMSProp', loss='binary_crossentropy', metrics=['acc'])
+print("\nStart training...")
+start_time = datetime.datetime.now()
+model_with_gru.fit(X_train, y_train, batch_size=256, epochs=5, validation_data=(X_val, y_val), verbose=2)
+time = datetime.datetime.now() - start_time
+print("\nTraining took {} seconds.".format(time))
+return model_with_gru
+
+
+
+
+model_with_gru.evaluate(X_test, y_test)
+
+def inser_own_input():
+
+    own_input = input("please write a sentence: ")
+    own_input_splitted = own_input.split()
+
+    own_input = [re.sub("@", "", word) for word in own_input_splitted]
+    own_input = [word.lower() for word in own_input]
+
+    own_input = [re.sub(r'[\W]', "", word) for word in own_input]
+    own_input = [re.sub('virginamerica', "", word) for word in own_input]
+    own_input = [re.sub('united', "", word) for word in own_input]
+    own_input = [re.sub('americanair', "", word) for word in own_input]
+    own_input
+
+    own_input = ' '.join(word for word in own_input if word not in stopwords.words('english'))
+    lemmatizer = WordNetLemmatizer()
+    own_input = ''.join([lemmatizer.lemmatize(word) for word in own_input])
+    #own_input = list(own_input)
+
+    input_with_padding = np.array(np.zeros(seq_len))
+    input_with_padding = input_with_padding.reshape(1, 26)
+    input_length = len([word for word in own_input.split()])
+    if input_length < seq_len:
+        n = seq_len - input_length
         zero_padding = list(np.zeros(n))
-        zero_padding = zero_padding + [dictio[word] for word in review.split()]
-        reviews_with_padding[idx] = zero_padding
+        zero_padding = zero_padding + [dictio[word] for word in own_input.split()]
+        input_with_padding[0] = zero_padding
+    print(model.predict(input_with_padding))
+
+    if model.predict_classes(input_with_padding) == 1:
+        print("positive")
     else:
-        reviews_with_padding[idx]= [dictio[word] for word in review.split()]
+        print("negative")
 
-
-reviews_with_padding = np.array(reviews_with_padding)
-reviews_with_padding.shape
-len(labels)
-
-X_train, X_test, y_train, y_test = train_test_split(reviews_with_padding, np.array(labels), test_size=0.1)
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2)
-X_train.shape
-y_train.shape
-
-from keras.models import Sequential
-from keras.layers import LSTM, Dense, Dropout, Masking, Embedding
-
-
-model = Sequential()
-
-# Embedding layer
-model.add(Embedding(len(list_of_words), 4000))
-#model.add(keras.layers.GlobalMaxPooling1D())
-model.add(LSTM(512, return_sequences=True, dropout=0.3, recurrent_dropout=0.1))
-model.add(LSTM(256, return_sequences=True, dropout=0.3, recurrent_dropout=0.1))
-model.add(LSTM(128, return_sequences=False, dropout=0.3, recurrent_dropout=0.1))
-model.add(keras.layers.Dropout(0.3))
-model.add(Dense(64, activation='relu'))
-model.add(keras.layers.Dropout(0.3))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-
-model.fit(X_train, y_train, batch_size=512, epochs=4, validation_data=(X_val, y_val), verbose=1)
-
-model.evaluate(X_test, y_test)
-
-own_input = input("please write a sentence")
-own_input = 'I have a@ girlfriend that I really love. she is the best.'
-own_input_splitted = own_input.split()
-
-own_input = [re.sub("@", "", word) for word in own_input_splitted]
-own_input = [word.lower() for word in own_input]
-
-own_input = [re.sub(r'[\W]', "", word) for word in own_input]
-own_input = [re.sub('virginamerica', "", word) for word in own_input]
-own_input = [re.sub('united', "", word) for word in own_input]
-own_input = [re.sub('americanair', "", word) for word in own_input]
-own_input
-
-own_input = ' '.join(word for word in own_input if word not in stopwords.words('english'))
-lemmatizer = WordNetLemmatizer()
-own_input = ''.join([lemmatizer.lemmatize(word) for word in own_input])
-own_input = list(own_input)
-
-input_with_padding = np.array(np.zeros(seq_len))
-input_with_padding = input_with_padding.reshape(1, 26)
-input_length = len([word for word in own_input.split()])
-if input_length < seq_len:
-    n = seq_len - input_length
-    zero_padding = list(np.zeros(n))
-    zero_padding = zero_padding + [dictio[word] for word in own_input.split()]
-    input_with_padding[0] = zero_padding
-
-if model.predict_classes(input_with_padding) == 1:
-    print("positive")
-else:
-    print("negative")
+inser_own_input()
